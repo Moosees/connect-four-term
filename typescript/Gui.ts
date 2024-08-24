dd4a1844e7918d16b8172ae50db8d7fafb9fffbb
Linus Almgren <linus.almgren@gmail.com>
2024-08-24 16:02:31 +0200

    add Gui paintStart method to render interactive main menu

--- typescript/Gui.ts
+++ typescript/Gui.ts
@@ -1,6 +1,20 @@
+import prompt from "inquirer-interactive-list-prompt";
 import { BoardMatrix } from "./Board.js";
 
 export default class Gui {
+  async paintStart() {
+    const answer = await prompt({
+      message: "Please select an option",
+      choices: [
+        { name: "Start game", value: "start", key: "s" },
+        { name: "Options", value: "options", key: "o" },
+        { name: "Quit", value: "quit", key: "q" },
+      ],
+    });
+
+    console.log(answer);
+  }
+
   paintBoard(board: BoardMatrix) {
     const boardString = board
       .reduce((acc, col) => {
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,7 +1,11 @@
-import Game from "./Game.js";
+// import Game from "./Game.js";
+import Gui from "./Gui.js";
 
-console.log("Create new Game");
-const game = new Game({});
+// console.log("Create new Game");
+// const game = new Game({});
+//
+// console.log("game.run");
+// game.run();
 
-console.log("game.run");
-game.run();
+const gui = new Gui();
+gui.paintStart();
